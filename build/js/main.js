3b429bf69219e4eaf692fd54d4aae6998d5caff6
Andre Gaisinski <andygaisinskii@gmail.com>
2018-06-15 16:09:29 +0300

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// one = () => {
+//     var x = 1;
+// }
+
+// oneOne = () => {
+//     var xx = 11;
+// }
+// two = () => {
+//     var y = 2;
+// }
+
+// const greetings = (name) => {
+//     return `hello ${name}`;
+// }
+
+
+var blue = document.getElementById('blue').innerHTML = 'Hello';
---
+++ src/js/two.js
@@ -0,0 +1,14 @@
+// two = () => {
+//     var y = 2;
+// }
+
+// const greetings = (name) => {
+//     return `hello ${name}`;
+// }
+
+
+const blue = document.getElementById('blue').innerHTML = 'Hello';
+
+
+
+
